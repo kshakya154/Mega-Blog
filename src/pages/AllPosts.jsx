a454399a61cf935607ba7b505ca3997e06dfcb56
kshakya154 <kshakya154@gmail.com>
2025-03-05 11:46:37 +0530

    fix all posts button

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -4,11 +4,12 @@ import { Container } from "../components";
 import { PostCard } from "../components";
 function AllPosts() {
   const [posts, setPosts] = useState([]);
-  useEffect(() => {}, []);
-  appwriteService.getPosts([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents);
-    }
+  useEffect(() => {
+    appwriteService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    }, []);
   });
 
   return (
@@ -17,7 +18,7 @@ function AllPosts() {
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
-              <PostCard post={post} />
+              <PostCard {...post} />
             </div>
           ))}
         </div>
